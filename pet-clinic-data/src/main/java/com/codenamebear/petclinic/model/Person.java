c9f46c8f38f504386bd13eee37ab0c43adafbbca
MatthewDBrown50 <matthew.d.brown50@gmail.com>
2022-10-11 19:23:39 -0400

    adding base entity

--- pet-clinic-data/src/main/java/com/codenamebear/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/codenamebear/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.codenamebear.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/codenamebear/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/codenamebear/petclinic/model/Vet.java
@@ -1,4 +1,4 @@
 package com.codenamebear.petclinic.model;
 
-public class Vet extends Person{
+public class Vet extends Person {
 }
