126ef4dab8f3d5b7ef6699078ff639b9957d5a1d
MatthewDBrown50 <matthew.d.brown50@gmail.com>
2022-10-13 07:31:32 -0400

    implementing map-based services

--- pet-clinic-data/src/main/java/com/codenamebear/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/codenamebear/petclinic/services/OwnerService.java
@@ -1,10 +1,11 @@
 package com.codenamebear.petclinic.services;
 
 import com.codenamebear.petclinic.model.Owner;
+import com.codenamebear.petclinic.services.map.OwnerServiceMap;
 
 import java.util.Set;
 
-public interface OwnerService extends CrudService<Owner, Long> {
+public interface OwnerService extends OwnerServiceMap<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
--- pet-clinic-data/src/main/java/com/codenamebear/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/codenamebear/petclinic/services/PetService.java
@@ -2,8 +2,6 @@ package com.codenamebear.petclinic.services;
 
 import com.codenamebear.petclinic.model.Pet;
 
-import java.util.Set;
-
 public interface PetService extends CrudService<Pet, Long> {
 
 }
